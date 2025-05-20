dafbb22616414a00eba9e395192e9d06f3eed37b
WILSON ALBERTO MORALES LAZARO <wilsonm57wm@gmail.com>
2025-05-20 00:27:20 -0600

    [Update]: Se ha agregado el link en el home

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -7,12 +7,14 @@ import { RouterLink } from 'vue-router'
             <RouterLink class="link" to="/">Home</RouterLink>
             <RouterLink class="link" to="/antojitoselcentro">Antojitos</RouterLink>
             <RouterLink class="link" to="/prueba">Prueba</RouterLink>
+             <RouterLink class="link" to="/flip">Correcto</RouterLink>
         </nav>
     </header>
 </template>
 
 <style>
 .link{
+    margin: 2px 12px;
   color: white;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
+import NavigationBar from '@/components/NavigationBar.vue';
 </script>
 
 <template>
   <main>
+    <NavigationBar />
    <h1>Hola</h1>
   </main>
 </template>
